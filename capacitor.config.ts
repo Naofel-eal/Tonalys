898d90331dc60f52f09ab9343115d6bc122531ed
Naofel EL ALOUANI <naofel.eal@gmail.com>
2025-06-07 16:10:02 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Tonalys',
+  webDir: 'www'
+};
+
+export default config;
