c7b24e82b121f64aaff2993a3c76043069844f36
Naofel EL ALOUANI <naofel.eal@gmail.com>
2025-07-31 13:33:27 +0200

    ci: incremente version in package.json

--- src/app/infrastructure/web/pages/main-page/main-page.component.ts
+++ src/app/infrastructure/web/pages/main-page/main-page.component.ts
@@ -12,7 +12,7 @@ import { Scale } from 'src/app/domain/model/scale/scale';
 import { PianoOctaveComponent } from '../../shared/components/piano-octave/piano-octave.component';
 import { ListAllScalesUseCase } from '../../../../application/usecase/list-all-scales/list-all-scales.usecase';
 import { Router } from '@angular/router';
-import * as  packageJson from 'package.json';
+import { VERSION } from 'src/version';
 
 @Component({
   selector: 'app-main',
@@ -30,7 +30,7 @@ import * as  packageJson from 'package.json';
   ],
 })
 export class MainPageComponent implements OnInit {
-  public version = packageJson.version;
+  public version = VERSION;
   public scales$: Observable<Scale[]> = of([]);
 
   public constructor(
