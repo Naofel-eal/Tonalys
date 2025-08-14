ed4aabb6f44ab14dfac66e248c42b9459e31eee8
Naofel EL ALOUANI <naofel.eal@gmail.com>
2025-08-15 00:37:34 +0200

    refactor: use ionic in main component

--- src/infrastructure/web/app/main/main.component.ts
+++ src/infrastructure/web/app/main/main.component.ts
@@ -1,29 +1,29 @@
-import { Component, HostListener, inject } from '@angular/core';
-import { AsyncPipe, NgClass } from '@angular/common';
+import { Component, inject, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { Observable } from 'rxjs';
 import { PianoOctaveComponent } from '../piano-octave/piano-octave.component';
 import { ListAllScalesUseCase } from '../../../../application';
 import { Scale } from '../../../../domain';
-
-const SCROLL_THRESHOLD_BEFORE_REDUCE_HEADER_IN_PX = 100;
+import { IonContent, IonRouterLink } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { chevronBackOutline, chevronForwardOutline, handLeft, listOutline, musicalNotesOutline, refresh, star } from 'ionicons/icons';
 
 @Component({
   selector: 'app-main',
   standalone: true,
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css'],
-  imports: [AsyncPipe, RouterLink, PianoOctaveComponent, NgClass],
+  imports: [AsyncPipe, RouterLink, PianoOctaveComponent, IonContent, IonRouterLink],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class MainComponent {
   private readonly listAllScalesUseCase: ListAllScalesUseCase = inject(ListAllScalesUseCase);
   
   public readonly scales$: Observable<Scale[]> = this.listAllScalesUseCase.execute();
-  public headerReduced = false;
 
-  @HostListener('window:scroll', [])
-  public onWindowScroll() {
-    const scrollY = window.scrollY;
-    this.headerReduced  = scrollY > SCROLL_THRESHOLD_BEFORE_REDUCE_HEADER_IN_PX;
+  constructor() {
+    addIcons({ chevronForwardOutline, musicalNotesOutline, chevronBackOutline, listOutline, star, handLeft, refresh });
+    
   }
 }
